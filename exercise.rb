24afb3d8cc93f795f9439685997c982196ca8604
StephenVarela <varelast92@gmail.com>
2018-10-15 13:01:53 -0400

    did checks fizbuzz format

---
+++ exercise.rb
@@ -0,0 +1,13 @@
+data = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+data.each do |word|
+  if(word.downcase == word && word.length > 4)
+    puts "long and lowercase"
+  elsif(word.downcase == word)
+    puts "lowercase"
+  elsif(word.length > 4)
+    puts "long"
+  else
+    puts "#{word}"
+  end
+end
